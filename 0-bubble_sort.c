3d94f5b2fc2c94650f92d48c9d254fbca7ae73f6
waffle <asuka9767@protonmail.com>
2020-02-05 04:38:44 +0000

    uploading header file & problem 0 solution

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Entry Point
+ * @array: array of unordered ints
+ * @size: size of the array
+ * Description: sorts an array of ints using bubble sort)?
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+/**
+ * swap - swap ints
+ * @x: pointer to int in array
+ * @y: pointer to int in array
+ * Description: swaps int in the array)?
+ */
+
+void swap(int *x, int *y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
