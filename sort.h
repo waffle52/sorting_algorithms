bac69b861130e2f7349f18e0ae83d66ca1113d0e
waffle <asuka9767@protonmail.com>
2020-02-06 07:49:32 +0000

    added new function prototypes

--- sort.h
+++ sort.h
@@ -22,6 +22,8 @@ void swap(int *x, int *y);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, int low, int high);
+void partition_controller(int *array, int low, int high);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
