a7d9ed4ac0f9511e17128ccec48aac03c5689852
waffle <asuka9767@protonmail.com>
2020-02-07 01:18:20 +0000

    changed program to use Knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,29 +10,29 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, j, i;
+	size_t gap = 1, i, j;
 	int temp;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (gap < size)
+	while (gap <= size / 3)
 	{
 		gap = gap * 3 + 1;
 	}
 
-	for (gap = size / 2; gap > 0; gap /= 3)
+	while (gap > 0)
 	{
-
-		for (i = gap; i < size; i += 1)
+		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
-
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 			{
 				array[j] = array[j - gap];
 			}
 			array[j] = temp;
 		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
 	}
 }
